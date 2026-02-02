46d6ac68275d8ca2c29bb88be9ceea66f509729a
LeistonH <232432491+Leiston09@users.noreply.github.com>
2026-02-01 21:10:05 -0500

    Aprendiendo Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 
   <div class=" container mx-auto mt-20 bg-green-100">
     <Headers/>
+
+    <div class=" mt-12 md:flex bg-amber-300">
+      <Formulario/> 
+
+    </div>
+
   </div>
 
 
@@ -10,6 +16,7 @@
 
 <script setup>
 import Headers from './components/Headers.vue';
+import Formulario from './components/Formulario.vue';
 </script>
    
 <style scoped>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-
+    <div class="md:w-1/2">
+        <h2 class=" font-black text-3xl text-center">Seguimiento Pacientes</h2>
+        
+        <p class=" text-lg mt-5  mb-5 text-center">
+            Añadir pacientes y 
+            <samp class=" text-indigo-600 font-bold">Adminístrarlos</samp>
+        </p>
     </div>
 </template>
 
