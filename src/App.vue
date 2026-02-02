c1f74f0de6c515908f90d5490a454e5041eba1e6
LeistonH <232432491+Leiston09@users.noreply.github.com>
2026-02-01 20:21:58 -0500

    Proyecto Administrar pacientes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+
+  <div class=" container mx-auto mt-20 bg-green-100">
+    <Headers/>
+  </div>
+
+
+</template>
+
+<script setup>
+import Headers from './components/Headers.vue';
+</script>
+   
+<style scoped>
+
+</style>
